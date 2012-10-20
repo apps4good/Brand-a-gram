1d418bf3a582729001f434821698220dbb83a771
Dale Zak <dalezak@gmail.com>
2012-10-20 14:26:54 -0600

    Initial repo

---
+++ Classes/Delegates/A4GAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  A4GAppDelegate.h
+//  Brandagram
+//
+//  Created by Dale Zak on 12-10-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A4GAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Resources/Misc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Brandagram
+//
+//  Created by Dale Zak on 12-10-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "A4GAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([A4GAppDelegate class]));
+    }
+}
